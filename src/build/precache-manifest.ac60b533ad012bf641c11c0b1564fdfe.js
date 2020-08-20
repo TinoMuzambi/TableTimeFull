137ac66f4120f9a7a5fc088f32dbadfff1d92331
Tino <tinomuzambi@gmail.com>
2020-08-20 02:06:06 +0200

    Updates more front end

---
+++ src/build/precache-manifest.ac60b533ad012bf641c11c0b1564fdfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541bab15f3e0e8a706faf691ed24dfb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c076faf97a6d88d4f801",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "900e8f62b6482b25aeb3",
+    "url": "/static/css/main.5d114852.chunk.css"
+  },
+  {
+    "revision": "c076faf97a6d88d4f801",
+    "url": "/static/js/2.d49a9ac2.chunk.js"
+  },
+  {
+    "revision": "2f90de1fa437077acc84e59746ddc970",
+    "url": "/static/js/2.d49a9ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "900e8f62b6482b25aeb3",
+    "url": "/static/js/main.04eb2d16.chunk.js"
+  },
+  {
+    "revision": "4983f04cf3f0f241770c",
+    "url": "/static/js/runtime-main.03508d1f.js"
+  },
+  {
+    "revision": "d90b3bff9221bde91893bbc7a5083756",
+    "url": "/static/media/LexendDeca-Regular.d90b3bff.ttf"
+  }
+]);
