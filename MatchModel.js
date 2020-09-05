fdcc3625ed39af9d00013dc136ad0c546ff05656
Tino <tinomuzambi@gmail.com>
2020-09-05 11:52:34 +0200

    Updates match schema.

--- MatchModel.js
+++ MatchModel.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 const MatchSchema = mongoose.Schema({
-	id: String,
-	date: Date,
-	bestOf: String,
-	gameType: Number,
-	player1: String,
-	player2: String,
-	player1Score: Array,
-	player2Score: Array,
-	winner: String,
+	id: { type: String, required: true },
+	date: { type: Date, required: true },
+	bestOf: { type: String, required: true },
+	gameType: { type: Number, required: true },
+	player1: { type: String, required: true, default: "Player1" },
+	player2: { type: String, required: true, default: "Player2" },
+	player1Score: { type: Array, required: true },
+	player2Score: { type: Array, required: true },
+	winner: { type: String, required: true },
 });
 
 export default mongoose.model("tabletime", MatchSchema);
