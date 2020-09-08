276423cb6eab8c48002bd0d9e85d83748097619c
Tino <tinomuzambi@gmail.com>
2020-09-08 04:09:57 +0200

    Cleans up code.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 import Matches from "./MatchModel.js";
 import Pusher from "pusher";
 
