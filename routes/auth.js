da0f309332af0e7dbee8c0744178d9580a6dae16
Tino <tinomuzambi@gmail.com>
2020-09-14 11:07:24 +0200

    Saving user to database.

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,19 @@
 import express from "express";
 const router = express.Router();
-import User from "../models/UserModel";
+import User from "../models/UserModel.js";
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
 	const user = new User({
 		username: req.body.username,
 		password: req.body.password,
 	});
+
+	try {
+		const savedUser = await user.save();
+		res.status(201).send(savedUser);
+	} catch (err) {
+		res.status(400).send(err, "Something went wrong.");
+	}
 });
 
 export default router;
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 
