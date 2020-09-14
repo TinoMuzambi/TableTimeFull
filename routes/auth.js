d246f02a0ed93e354787f443491a53581f874ed7
Tino <tinomuzambi@gmail.com>
2020-09-14 20:34:08 +0200

    Integrates bcrypt.

--- routes/auth.js
+++ routes/auth.js
@@ -2,19 +2,31 @@ import express from "express";
 const router = express.Router();
 import User from "../models/UserModel.js";
 import { registerValidation, loginValidation } from "../validation.js";
+import bcrypt from "bcryptjs";
 
 // Register route.
 router.post("/register", async (req, res) => {
+	// Validate the data first.
 	const { error } = registerValidation(req.body);
 	if (error) {
 		return res.status(400).send(error.details[0].message);
 	}
 
+	// Check if user exists.
+	const userExists = await User.findOne({ username: req.body.username });
+	if (userExists) return res.status(400).send("Username already taken.");
+
+	// Hash passowrds
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+	// Create the new user.
 	const user = new User({
 		username: req.body.username,
-		password: req.body.password,
+		password: hashedPassword,
 	});
 
+	// Save the user.
 	try {
 		const savedUser = await user.save();
 		res.status(201).send(savedUser);
