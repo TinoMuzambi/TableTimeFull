bcfd6022a0c039fda0a43138502bccd5720e60c4
Tino <tinomuzambi@gmail.com>
2020-09-16 19:56:03 +0200

    Adds password confirm validation.

--- validation.js
+++ validation.js
@@ -5,6 +5,7 @@ export const registerValidation = (data) => {
 	const schema = Joi.object({
 		username: Joi.string().min(3).required(),
 		password: Joi.string().min(8).required(),
+		passwordConfirm: Joi.string().equal(Joi.ref("password")).required(),
 		userType: Joi.string().allow(""),
 	});
 	return schema.validate(data);
